88b493fcbbf38fd9374718e2c2c519e7f1ed9f7a
Maylien <littlemaylien@gmail.com>
2019-12-15 11:25:48 +0900

    Physics

--- data/data.js
+++ data/data.js
@@ -271,7 +271,7 @@ export const labdata = [
     "labname": "Purkait Laboratory",
     "category": "Mathematics",
     "department": "School of Science - Department of Mathematics",
-    "title": "Discovering "New" operators",
+    "title": "Discovering \"New\" operators",
     "subtitle": "We study local Hecke algebras of the covering groups and characterisation of the newspace of half-integral weight modular forms.",
     "url": "http://www.math.titech.ac.jp/~purkait/",
     "photo": "https://educ.titech.ac.jp/math/eng/faculty/research_lab/img/lab_20850_purkait.jpg",
@@ -342,7 +342,7 @@ export const labdata = [
     "labname": "Kato Laboratory",
     "category": "Mathematics",
     "department": "School of Science - Department of Mathematics",
-    "title": ""Analytic" approach to arithmetic geometry",
+    "title": "\"Analytic\" approach to arithmetic geometry",
     "subtitle": "Our interests lie in the foundations of non-Archimedean geometry - so-called, rigid geometry - and its application to arithmetic/algebraic geometry.",
     "url": "http://www.math.titech.ac.jp/~bungen/index-e.html",
     "photo": "https://educ.titech.ac.jp/math/eng/faculty/research_lab/img/lab_10396_kato.jpg",
@@ -415,13 +415,12 @@ export const labdata = [
     "labname": "Kuze Group",
     "category": "Physics",
     "department": "School of Science - Department of Physics",
-    "title": "We are mainly interested in curves and surfaces",
-    "subtitle": "We are working on differential geometry, and interested in maximal sufaces in Lorentzian 3-space and isometric deformations of singularities.",
-    "url": "http://www.is.titech.ac.jp/~umehara/",
+    "title": "Exploring the ultimate constituents",
+    "subtitle": "What are our bodies, the stars, and all things around us made of? What are the forces that act between them? The study of particle physics pursues such fundamental questions.",
+    "url": "http://www-kuze.phys.titech.ac.jp/",
     "photo": "https://educ.titech.ac.jp/bio/eng/faculty/research_lab/img/lab_10396_kimura.jpg",
     "keywords": [
-                "Manifolds",
-                "Curves and surfaces",
+                "High energy physics",
     ],
     "active": true
 　　},
@@ -429,6 +428,20 @@ export const labdata = [
     {
     "labname": "Satoh Laboratory",
     "category": "School of Science - Department of Physics",
+    "keywords": [
+                "Ultrafast spectroscopy",
+    ],
+    "active": true
+　　},
+
+    {
+    "labname": "Satoh Laboratory",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "Control of magnetic wave by ultrashort light pulses",
+    "subtitle": "We study ultrafast coherant control of magnetic materials by use of femtosecond light pulses, and understanding of the mechanisms for pioneering \"optomagnonics.\"",
+    "url": "https://satoh.phys.titech.ac.jp/en/",
+    "photo": "https://educ.titech.ac.jp/phys/eng/faculty/research_lab/img/lab_22491_satoh.jpg",
     "keywords": [
                 "Ultrafast spectroscopy",
     ],
@@ -445,11 +458,41 @@ export const labdata = [
 　　},
 
     {
+    "labname": "Sayuma Group",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "Challenging important problems in cosmology",
+    "subtitle": "There are many unsolved problems in cosmology such as dark matter, dark energy, black hole, structures in the Universe, and so on. We address these challenges by means of theoretical physics.",
+    "url": "http://www.th.phys.titech.ac.jp/cosmo/",
+    "photo": "https://educ.titech.ac.jp/math/eng/faculty/research_lab/img/lab_10396_miura.jpg",
+    "keywords": [
+                "Black hole",
+    ],
+    "active": true
+　　},
+
+
+    {
     "labname": "Notomi Laboratory",
     "category": "School of Science - Department of Physics",
     "keywords": [
                 "Nanomaterial",
-                "Metamaterial"
+                "Metamaterial",
+    ],
+    "active": true
+　　},
+
+    {
+    "labname": "Notomi Laboratory",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "Exploring novel optical physics by nano-scale artificial structures",
+    "subtitle": "Investigating various innovation seismic resistant building systems with the aid of sophisticated numerical analysis and simulation techniques.",
+    "url": "http://notomi-lab.phys.titech.ac.jp/",
+    "photo": "https://educ.titech.ac.jp/phys/eng/faculty/research_lab/img/lab_15501_notomi.jpg",
+    "keywords": [
+                "Nanomaterial",
+                "Metamaterial",
     ],
     "active": true
 　　},
@@ -464,8 +507,37 @@ export const labdata = [
 　　},
 
     {
+    "labname": "Koga Laboratory",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "Exploring quantum phenomena in many-body systems",
+    "subtitle": "We study theories of electric states in solids, with a particular focus on strongly correlated electron systems to clarify exotic properties.",
+    "url": "http://www.stat.phys.titech.ac.jp/koga/index-e.html",
+    "photo": "https://educ.titech.ac.jp/phys/eng/faculty/research_lab/img/lab_10396_koga.jpg",
+    "keywords": [
+                "Theoretical physics",
+    ],
+    "active": true
+　　},
+
+
+    {
     "labname": "Jinnouchi Group",
     "category": "School of Science - Department of Physics",
+    "keywords": [
+                "Tracking detector",
+    ],
+    "active": true
+　　},
+
+    {
+    "labname": "Jinnouchi Group",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "JLAB searches for the new particles at LHC ATLAS!",
+    "subtitle": "We are searching for the physics beyond the Standard Model, especially the origin of the dark matter through the LHC/ATLAS experiment.",
+    "url": "http://www-hep.phys.titech.ac.jp/jlab/index_e.html",
+    "photo": "https://educ.titech.ac.jp/phys/eng/faculty/research_lab/img/lab_10396_Jinnouchi.jpg",
     "keywords": [
                 "Tracking detector",
     ],
@@ -477,8 +549,24 @@ export const labdata = [
     "category": "School of Science - Department of Physics",
     "keywords": [
                 "Crystal growth",
-                "Scanning probe microscopy"
+                "Scanning probe microscopy",
     ],
     "active": true
 　　},
+
+    {
+    "labname": "Yoshino Laboratory",
+    "category": "Physics",
+    "department": "School of Science - Department of Physics",
+    "title": "Spintronics and applied novel materials",
+    "subtitle": "Our research aims to design and develop novel materials for spintronics, which is considered a prospective technology to replace current eletronics",
+    "url": "http://www.ss.phys.titech.ac.jp/",
+    "photo": "https://educ.titech.ac.jp/phys/eng/faculty/research_lab/img/lab_10396_yoshino.jpg",
+    "keywords": [
+                "Crystal growth",
+                "Scanning probe microscopy",
+    ],
+    "active": true
+　　},
+
 ]
