94287e8300512a0a2eaeadcdb18423ada2489923
kitokiyohito <srrzhuaktm@icloud.com>
2019-12-15 13:55:19 +0900

    TopPageのデザインを修正しました。

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,12 @@
   <!-- <img src="https://st2.depositphotos.com/4521519/7577/v/950/depositphotos_75770271-stock-illustration-old-chemistry-laboratory-pattern-set.jpg" /> -->
   
   <p>Co-Lab</p>
-
+  
   <form method="get" action="http://localhost:3000/result">
   <input class="main-search" type="text" id="name" name="name" placeholder="Freeword" autocomplete="off" required
        minlength="1" maxlength="25" size="30">
   </form>
-  <NLink to="/result?category=生物学" tag="button">生物学</NLink>
-  <NLink to="/result?category=物理学" tag="button">物理学</NLink>
-
-  <NLink to="/lifescience">
-   Result model
-  </NLink>
-
-
+  <v-btn class="category-button" color="primary" dark @click="opencategory">Category</v-btn>
   <v-btn
       class='about-us-button'
       color="primary"
@@ -24,40 +17,18 @@
     >
       About us
     </v-btn>
-    <v-card
-    flat
-    tile
-  >
- 
-              <v-btn
-                v-for="(social, i) in socials"
-                :key="i"
-                :color="social.color"
-                class="white--text"
-                fab
-                icon
-                small
-              >
-                <v-icon>{{ social.icon }}</v-icon>
-              </v-btn>
-
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
   <v-row justify="center">
     <v-dialog v-model="category" scrollable >
-      <template v-slot:activator="{ on }">
+      <v-card>
+        <template v-slot:activator="{ on }">
         <v-btn color="primary" dark v-on="on">Category</v-btn>
       </template>
-      <v-card>
         <v-card-title>Category</v-card-title>
         <v-divider></v-divider>
-        <v-card-text style="height: 300px;">
+        <v-card-text style="height: 500px;">
           <v-row justify="center">
             <v-expansion-panels inset>
-              <v-expansion-panel
+              <v-expansion-panel 
                 v-for="(category, i) in categories"
                 :key="i"
               >
@@ -78,7 +49,7 @@
                         :rules="[v => !!v || 'You must agree to continue!']"
                         v-bind:label="subCategory"
                         required
-                        @click="deback"
+                      
                       ></v-checkbox>
                     </v-col>
                     
@@ -90,10 +61,12 @@
      
         </v-card-text>
         <v-divider></v-divider>
-        <v-card-actions>
+        <v-card-actions >
           <v-btn 
           class = 'research-button'
-          color="blue darken-3" text @click="submit">Research</v-btn>
+          color="blue darken-3" 
+          height="80"
+          text @click="submit">Research</v-btn>
       
         </v-card-actions>
       </v-card>
@@ -148,6 +121,7 @@ export default {
     Logo,
     VuetifyLogo
   },
+     layout:"top",
      data () {
       return {
         dialogm1: '',
@@ -201,8 +175,9 @@ export default {
 
         return Math.floor(Math.random() * (max - min + 1)) + min
       },
-      deback (){
+      opencategory (){
         console.log(this.checkboxes)
+        this.category =true
       },
       submit(){
         const checkboxes=this.checkboxes.join(',')
@@ -217,25 +192,22 @@ export default {
 
 <style>
  .example {
-  /* position: relative; */
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 100px;
     width: 100%;
-    height: 100%;
-    left: 0;
-    position: absolute;
+    height: 100vh;
     background: url('https://st2.depositphotos.com/4521519/7577/v/950/depositphotos_75770271-stock-illustration-old-chemistry-laboratory-pattern-set.jpg') no-repeat center center;
     background-size: cover;
   }
 
 .example p {
-  position: absolute;
+  position: relative;
   color: white;/*文字は白に*/
   font-weight: bold; /*太字に*/
-  font-size: 10em;/*サイズ2倍*/
-  top: 30%;
-  left: 50%;
-  -ms-transform: translate(-50%,-50%);
-  -webkit-transform: translate(-50%,-50%);
-  transform: translate(-50%,-50%);
+  font-size: 9em;/*サイズ2倍*/
   margin:0;
   padding:0;
   /*文字の装飾は省略*/
@@ -255,23 +227,22 @@ label {
 }
 
 input.main-search {
-    position: absolute;
-    top: 45.3%;
-    left: 30%;
+    position: relative;
     margin: .4rem 0;
     border: solid black 2px; /*線で囲う*/
-    font-size: 33px;/*文字サイズ*/
+    font-size: 27px;/*文字サイズ*/  
     background-color:white;
     color: black;/*文字色*/
 }
 
+.category-button {
+  margin-top: 20px;
+}
 .about-us-button {
-    position: absolute;
-    top: 40%;
-    left: 70%;
+  margin-top: 50px;
 }
 .research-button {
-    float: center;
+     float: center;
 }
 .v-input--selection-controls__ripple  {
     color:black  inherit
